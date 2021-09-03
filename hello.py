a451e54f61ae11970175e11ca0c79ba282cf5c68
Cade Brek <brekkenc25@greenhill.org>
2021-09-03 13:07:49 -0500

    This is commit

---
+++ hello.py
@@ -0,0 +1,25 @@
+# For Comments
+
+#Cade Brekken
+#Date: 09/03/21
+#This is our first program test average
+
+#Set variables
+test1=92
+test2=89
+test3=91
+test4=87
+
+#declare a avarage variable
+average=0
+print(type(average))
+#Four basi)c operators
+# sum +
+#sub -
+#mult *
+#div /
+average= (test1+test2+test3+test4)/4
+# Lets print results
+print(type(average))
+#command  using keywords
+print("The tests average is",average)
