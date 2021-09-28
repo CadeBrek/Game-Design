e346ffcd308310dc25695c2936d222e14f226921
Cade Brek <brekkenc25@greenhill.org>
2021-09-28 13:04:25 -0500

    9/28

--- Wordgame.py
+++ Wordgame.py
@@ -1,31 +1,22 @@
 import os, random
 os.system('cls')
 
-name=input("What is your name? ")
-print( "Hi, ",name)
-answer=input("Would you like to play my game ")
-while ('y' in answer):
-    randint = ("Orange,Apple,Banana")
-    randy=random.randint
-    print(randy)
-    counter=0
-    guess=input("please enter an integer ")
-while(guess != randy and counter < 5):
-    try:
-        guess=int(guess)
-        check=True
-    except ValueError:
-        check=False
-if(check):
-    if (guess == randy):
-            print("you win! # of tries: ", counter+1)
+#Guessing Game
+
+compParts=["keyboard", "printer", "motherboard", "CPU", "fans", "storage"]
+
+word= random.choice(compParts)
+word = word.lower()
+print(word)
+for letter in word:
+    print("_ ", end = " ")
+guess=input("guess the word I am thinking ")
+while (guess not in word):
+    guess= guess.lower
+    if guess in word:
+        print ("Good Job")
+        break
     else:
-        print("sorry! You lost, try again")
-counter +=1
-if guess > randy:
-    print("you guessed too high")
-else:
-    print("you guessed too low")
-guess=input("please guess again ")
-answer=input("Do you want to play again?")
-print("Thank you for playing the game")
+        print(" Wrong ")
+        guess=input("try again ")
+print("thank you for playng")
