c6b1670bfb9f021985c8e4bc39cc4168c547277d
Cade Brek <brekkenc25@greenhill.org>
2021-10-04 12:34:56 -0500

    10/4 Quiz

---
+++ Quiz.py
@@ -0,0 +1,43 @@
+#Cade Brekken
+#10/04/21
+
+import os, random
+os.system('cls')
+
+#Question 1
+print(len('Hello world!'))
+
+#Question 2
+strString = 'Study.com is a wonderful resource'
+print(strString.find('great'))
+print(strString.find('wonderful'))
+print(strString.find('.'))
+
+#Question 3
+print(strString[15:24])
+
+#Question 4
+print[0]
+
+#Question 5
+print(strString[15:24].find('der')+len(strString))
+
+# Question 6
+var1=17
+var2=4
+result=var1 % var2
+print (result)
+
+#Question 7
+print("\t")
+
+#Question 8
+print("\n")
+
+#Question 9
+#The Comcatenation operator is +
+print(strString.find[15:24]) + (strString.find[1:4])
+
+# Question 10
+list1='0','1','2','3','4','6','7','8','9','10'
+list1.insert(5,'5')
